3f40eef371879c3ef33663ec382fc1bc8b47203d
Achyuth1 <achyuth6654@gmail.com>
2020-06-19 22:25:11 +0530

    updated to test

--- play.py
+++ play.py
@@ -1,15 +1,20 @@
 from Rubix.cube import Cube
-
+import time
 myCube = Cube()
-myCube.set_render_time(5)
+myCube.set_render_time(2)
 cmd = "x"
 while cmd:
 	myCube.rotate_face(cmd)
 	if cmd[-1] is not 's':
-		myCube.print_correct_cubies()
+		# myCube.print_correct_cubies()
 		myCube.render()
 		
 	print("SCORE: ", myCube.get_cube_score())
 	cmd = input("Enter the command: ")
+	
+myCube.reset()
+print("SCORE: ", myCube.get_cube_score())
+myCube.render()
+
 
 print("Exited the simulation")
--- test.py
+++ test.py
@@ -1,16 +1,24 @@
 from Rubix.cube import Cube
 
 myCube = Cube()
-# myCube.set_render_time(10)
-#
-# myCube.render()
+myCube.set_render_time(1)
+
+cmds = ['b', 'b', 'l', "u'", 'b', "f'", "b'", 'u', "d'", "d'",
+        'f', 'b', 'l', "u'", "l'", 'u', 'f', 'f', 'u', "d'"]
+
 print(myCube.get_cube_score())
-# cmd = "x"
-# while cmd:
-# 	cmd = input("Enter the coords csv: ")
-# 	# print(myCube.get_visible_colors([int(i) for i in cmd.split(",")]))
-# 	print(myCube.is_cubie_correct([int(i) for i in cmd.split(",")]))
+for cmd in cmds:
+	myCube.rotate_face(cmd)
+	print(myCube.get_cube_score())
+
+# myCube.render()
+#
+# for cmd in cmds[::-1]:
+# 	if cmd.endswith("'"):
+# 		cmd = cmd[:1]
+# 	else:
+# 		cmd += "'"
+# 	myCube.rotate_face(cmd)
+# 	print(myCube.get_cube_score())
 # 	myCube.render()
-#
-#
-# print("Exited the simulation")
+
